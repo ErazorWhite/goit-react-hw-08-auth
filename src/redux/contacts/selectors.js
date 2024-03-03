677d18fe42ee7d082876cc305b2f07b7a9a1cb90
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2024-03-03 21:39:19 +0200

    fuse search imp

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -1,5 +1,13 @@
 import { createSelector } from '@reduxjs/toolkit';
 import { selectFilter } from '../filter/selectors';
+import Fuse from 'fuse.js';
+
+const fuseOptions = {
+  minMatchCharLength: 1,
+  findAllMatches: true,
+  threshold: 0.2,
+  keys: ['name', 'number'],
+};
 
 export const selectContacts = state => state.contacts.items;
 export const selectIsLoading = state => state.contacts.loading;
@@ -8,11 +16,8 @@ export const selectError = state => state.contacts.error;
 export const selectFilteredContacts = createSelector(
   [selectContacts, selectFilter],
   (contacts, filter) => {
-    const normalizedFilter = filter?.toLowerCase();
-    if (!contacts.length) return;
-    if (!normalizedFilter) return contacts;
-    return contacts?.filter(({ name }) =>
-      name.toLowerCase().includes(normalizedFilter)
-    );
+    if (!filter) return contacts;
+    const fuse = new Fuse(contacts, fuseOptions);
+    return fuse.search(filter).map(result => result.item);
   }
 );
